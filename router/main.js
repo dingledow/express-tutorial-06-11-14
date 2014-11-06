c5387a1b6edab5c2c52025fc9ee6ff86b28808b0
David <david@ingledow.co.uk>
2014-11-06 16:03:25 +0000

    Trying out another tutorial

---
+++ router/main.js
@@ -0,0 +1,11 @@
+module.exports = function(app) {
+
+	app.get('/', function(request, response) {
+		response.render('index.html')
+	});
+
+	app.get('/about',function(request, response) {
+		response.render('about.html');
+	});
+
+}
